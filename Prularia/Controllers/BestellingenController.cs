387e9a5f30991c2084c9a633ed6f6bf1a887f35d
Simon <simon.bruyneel888@gmail.com>
2024-09-17 14:31:18 +0200

    fixed name problems with controllers and folders

--- Prularia/Controllers/BestellingenController.cs
+++ Prularia/Controllers/BestellingenController.cs
@@ -18,4 +18,34 @@ public class BestellingenController : Controller
         model.BestellingItems = await _bestellingService.getBestellingenAsync();
         return View(model);
     }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        Bestelling? b = await _bestellingService.GetAsync(id);
+        if (b == null) return NotFound();
+
+        BestellingDetailViewModel vm = new BestellingDetailViewModel()
+        {
+            BestelId = b.BestelId,
+            Besteldatum = b.Besteldatum,
+            Betaald = b.Betaald,
+            Betalingscode = b.Betalingscode,
+            Annulatie = b.Annulatie,
+            Annulatiedatum = b.Annulatiedatum,
+            Terugbetalingscode = b.Terugbetalingscode,
+            ActiecodeGebruikt = b.ActiecodeGebruikt,
+            Bedrijfsnaam = b.Bedrijfsnaam,
+            BtwNummer = b.BtwNummer,
+            BestellingsStatus = b.BestellingsStatus,
+            Betaalwijze = b.Betaalwijze,
+            FacturatieAdres = b.FacturatieAdres,
+            Voornaam = b.Voornaam,
+            Familienaam = b.Familienaam,
+            LeveringsAdres = b.LeveringsAdres,
+
+            Bestellijnen = b.Bestellijnen
+        };
+
+        return View(vm);
+    }
 }
