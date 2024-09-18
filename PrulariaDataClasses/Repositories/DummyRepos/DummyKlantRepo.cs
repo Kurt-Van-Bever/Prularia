5ee29cc7c840583a860d597ca026b07fedffe764
GERT THOMAS <GERT.THOMAS@vdabcampus.be>
2024-09-18 15:08:32 +0200

    Klantenlijst gemerged

--- PrulariaDataClasses/Repositories/DummyRepos/DummyKlantRepo.cs
+++ PrulariaDataClasses/Repositories/DummyRepos/DummyKlantRepo.cs
@@ -4,7 +4,17 @@ namespace Prularia.Repositories;
 
 public class DummyKlantRepo : IKlantRepo
 {
+    public Klant? Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
     public Task<List<Klant>> GetKlantenAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Klant klant)
     {
         throw new NotImplementedException();
     }
