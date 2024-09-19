c010b108d323c40ae1571060e16486efa943b14b
Simon <simon.bruyneel888@gmail.com>
2024-09-19 13:16:02 +0200

    created securitycontroller

---
+++ Prularia/Controllers/SecurityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prularia.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Prularia/Filters/SessionAuthorizationAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Prularia.Filters
+{
+    public class SessionAuthorizationAttribute : AuthorizeAttribute
+    {
+        private const string SESSION_LOGGEDIN_USER = "LOGGEDIN_USER";
+
+        
+    }
+}
