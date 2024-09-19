b6c3e9ae9ae021568381ec880f88f8f85d584596
GERT THOMAS <GERT.THOMAS@vdabcampus.be>
2024-09-19 15:54:59 +0200

    Masking geïmplementeerd

--- Prularia/Models/PaswoordViewModel.cs
+++ Prularia/Models/PaswoordViewModel.cs
@@ -4,13 +4,17 @@ namespace Prularia.Models;
 
 public class PaswoordViewModel
 {
-    [Required]
     [Display(Name = "Paswoord")]
-    public string OudPaswoord {  get; set; }
-    [Required]
+    [Required(ErrorMessage = "Verplicht")] //Deze errormessage wordt niet doorgegeven om een of andere reden
+    public string? OudPaswoord {  get; set; }
+
     [Display(Name = "Nieuw paswoord")]
-    public string NieuwPaswoord {  get; set; }
-    [Required]
+    [Required(ErrorMessage = "Verplicht")] //Deze errormessage wordt niet doorgegeven om een of andere reden
+    [StringLength(int.MaxValue, ErrorMessage = "Te kort", MinimumLength = 8)]
+    public string? NieuwPaswoord {  get; set; }
+
     [Display(Name = "Confirmeer nieuw paswoord")]
-    public string NieuwPaswoordConfirmatie {  get; set; }
+    [Required(ErrorMessage = "Verplicht")] //Deze errormessage wordt niet doorgegeven om een of andere reden
+    [StringLength(int.MaxValue, ErrorMessage = "Te kort", MinimumLength = 8)]
+    public string? NieuwPaswoordConfirmatie {  get; set; }
 }
