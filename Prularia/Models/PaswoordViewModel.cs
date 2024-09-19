8f49a4b3c856a194f42070212e9aad42a5adbb33
GERT THOMAS <GERT.THOMAS@vdabcampus.be>
2024-09-19 16:04:44 +0200

    probleempjes met errormessage in annotations

--- Prularia/Models/PaswoordViewModel.cs
+++ Prularia/Models/PaswoordViewModel.cs
@@ -5,16 +5,16 @@ namespace Prularia.Models;
 public class PaswoordViewModel
 {
     [Display(Name = "Paswoord")]
-    [Required(ErrorMessage = "Verplicht")] //Deze errormessage wordt niet doorgegeven om een of andere reden
+    [Required(ErrorMessage = "Verplicht")] 
     public string? OudPaswoord {  get; set; }
 
     [Display(Name = "Nieuw paswoord")]
-    [Required(ErrorMessage = "Verplicht")] //Deze errormessage wordt niet doorgegeven om een of andere reden
-    [StringLength(int.MaxValue, ErrorMessage = "Te kort", MinimumLength = 8)]
+    [Required(ErrorMessage = "Verplicht")] 
+    //[StringLength(int.MaxValue, ErrorMessage = "Te kort", MinimumLength = 8)]
     public string? NieuwPaswoord {  get; set; }
 
     [Display(Name = "Confirmeer nieuw paswoord")]
-    [Required(ErrorMessage = "Verplicht")] //Deze errormessage wordt niet doorgegeven om een of andere reden
-    [StringLength(int.MaxValue, ErrorMessage = "Te kort", MinimumLength = 8)]
+    [Required(ErrorMessage = "Verplicht")] 
+    //[StringLength(int.MaxValue, ErrorMessage = "Te kort", MinimumLength = 8)]
     public string? NieuwPaswoordConfirmatie {  get; set; }
 }
