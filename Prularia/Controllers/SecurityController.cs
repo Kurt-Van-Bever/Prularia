fd4464fd13a439bb26c6a8816701a691c657aa2f
JelseM <jelse.martens@vdabcampus.be>
2024-09-26 13:01:27 +0200

    Dropdown securitygroep maar werkt niet

--- Prularia/Controllers/SecurityController.cs
+++ Prularia/Controllers/SecurityController.cs
@@ -186,6 +186,7 @@ namespace Prularia.Controllers
         public IActionResult PersoneelToevoegen()
         {
             var vm = new PersoneelToevoegenViewModel();
+            vm.Securitygroepen = _securityService.GetAllSecurityGroepen() ?? new List<Securitygroep>(); ;
             return View(vm);
         }
 
@@ -203,6 +204,7 @@ namespace Prularia.Controllers
                 lid.Voornaam = vm.Voornaam;
                 lid.Familienaam = vm.Familienaam;
                 lid.InDienst = vm.InDienst;
+                lid.SecurityGroepen.Add(_securityService.GetSecuritygroep(vm.SelectedSecuritygroepId));
                 _securityService.PersoneelslidToevoegen(account, lid);
             }
             return RedirectToAction("Index");
